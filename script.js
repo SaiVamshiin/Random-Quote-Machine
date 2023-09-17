a19d8f48d64b3aebfb110edbbce677d0ce2402a7
B Sai Vamshi <134629654+SaiVamshiin@users.noreply.github.com>
2023-09-17 21:03:04 +0545

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import React from 'https://esm.sh/react@18.2.0';
 import ReactDOM from 'https://esm.sh/react-dom@18.2.0';
 
 const quoteData = [
-{ text: "“If you want to be sucessful, put EXTRA EFFORT.”", author: "Sai Vamshi" },
+{ text: "“If you want to be sucessful, put EXTRA EFFORT.”", author: "Sai Vamshi" }, 
 { text: "“The purpose of our lives is to be happy.”", author: "Dalai Lama" },
 { text: "“Life is what happens when you're busy making other plans.”", author: "John Lennon" },
 { text: "“Get busy living or get busy dying.”", author: "Stephen King" },
